47a9d855f6b868354195bcf9dafb5a15df1b6c24
Zach Carrera <zachcarrera@gmail.com>
2023-03-22 22:23:36 -0700

    fix validations in jobs model

--- server/models/job.model.js
+++ server/models/job.model.js
@@ -4,13 +4,13 @@ const JobSchema = new mongoose.Schema(
     {
         title: {
             type: String,
-            required: [true, "Name is a required field."],
-            minlength: [3, "Name must be atleast 3 characters."],
+            required: [true, "Title is a required field."],
+            minlength: [3, "Title must be atleast 3 characters."],
         },
         company: {
             type: String,
-            required: [true, "Name is a required field."],
-            minlength: [3, "Name must be atleast 3 characters."],
+            required: [true, "Company is a required field."],
+            minlength: [3, "Company must be atleast 3 characters."],
         },
         salary: {
             type: Number,
