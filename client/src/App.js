3d7a76d42bfa1e18f8f4680ec0762d003ca41b60
Zach Carrera <zachcarrera@gmail.com>
2023-03-23 07:28:35 -0700

    add routes for all views in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { JobsList } from "./views/JobsList";
+import { OneJob } from "./views/OneJob";
+import { NewJob } from "./views/NewJob";
+import { EditJob } from "./views/EditJob";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Jobs Board</h1>
+            <Routes>
+                <Route path="/" element={<JobsList />} />
+                <Route path="/jobs/new" element={<NewJob />} />
+                <Route path="/jobs/edit/:_id" element={<EditJob />} />
+                <Route path="/jobs/:_id" element={<OneJob />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
